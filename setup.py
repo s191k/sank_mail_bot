9b5a6b765a676c31adc3017311866fa4e7b35aba
Kupirev Alexander <solnushkoon@gmail.com>
2019-07-21 11:18:25 +0300

    rework add setup.py + Dockerfile

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-#https://klen.github.io/create-python-packages.html
 from setuptools import setup, find_packages
 
-# Загружаем зависимости из файла
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-# readme = open('README.md').read()
-# with open('README.md') as r:
-#     readme_file = r.read()
+required = [
+    'pyTelegramBotAPI == 3.6.6',
+    'requests == 2.22.0'
+]
 
 setup(
-    name='sank_mail_bot',
+    name='sank_mail_bot_p',
     version=0.1,
     author='Sank(s191k) -- Kupyrev Alexander',
     author_email='solnushkoon@mail.ru',
     url='https://github.com/s191k/sank_mail_bot',
-    description='Simple bot for work with emails in telegram chat', ## Summary
+    description='Simple bot for work with emails in telegram chat',
     # long_description=readme_file, ## Ругается на русский
     install_requires=required,
     packages=find_packages()
